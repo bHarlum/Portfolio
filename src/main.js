02e0e97eff93a7d7edd2e52ba45416438e6c456e
Bryce harlum <bryce.harlum@gmail.com>
2019-03-22 11:49:43 +1100

    added table to base of page, created a nav hamburger menu, made links within hamburger functional

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 
 window.addEventListener("scroll", function() {
     let world = document.getElementById("world");
-    console.log("running")
     world.style.transform = "rotate("+(window.pageYOffset/4)+"deg)";
-    world.style.top = "50%";
 });
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "150px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
 
