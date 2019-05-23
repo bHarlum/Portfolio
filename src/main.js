5376cffd757027c87fd8012ec96306a57ed2e938
Bryce Harlum <bryce.harlum@gmail.com>
2019-05-23 10:14:15 +1000

    updated some styling, created a card to display git hub project, created a api request in javascript main file to fill in github card.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
+// Populates the repo cards on the home section of page.
+// Takes an array of repo names.
 
-
+const Http = new XMLHttpRequest();
+  const url='https://api.github.com/repos/bharlum/calculator';
+  Http.open("GET", url);
+  Http.send();
+  Http.onload=()=>{
+    obj = JSON.parse(Http.responseText);
+      //Print your git data here
+      //document.getElementById('git-card-list').innerHTML = obj["name"] + ": " + obj["html_url"];
+      let ul = document.getElementById("git-card-list");
+      //name of project:
+      let li = document.createElement("li");
+      li.setAttribute('id',obj["name"]);
+      li.appendChild(document.createTextNode(obj["name"]));
+      ul.appendChild(li);
+      // project description:
+      li = document.createElement("li");
+      li.setAttribute('id',obj["name"] + "description");
+      li.appendChild(document.createTextNode(obj["description"]));
+      ul.appendChild(li);
+      // project picture:
+      //make background image an image of program.
+  }
 
 window.addEventListener("scroll", function() {
     let world = document.getElementById("world");
@@ -10,7 +33,7 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "150px";
   }
   
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
 
